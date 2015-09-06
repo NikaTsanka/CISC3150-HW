4885cd79f1a0436e65d90ae56238e01d453cb8ee
NikaTsanka <nikatsanka@outlook.com>
2015-09-06 00:05:51 -0400

    Late night draft

--- JAVA - HW2/src/Q1.java
+++ JAVA - HW2/src/Q1.java
@@ -1,4 +1,5 @@
 
+
 public class Q1 {
 
 	public static void main(String[] args) {
@@ -6,9 +7,25 @@ public class Q1 {
 		System.out.println("Test 1");
 		System.out.println("Hi again");
 		
-		//Push test
+		int range = 5;
 		
+		int count = 0;
 		
+		for (int i = 1; i <= range; i++) {
+			printSpaces(((range + 1) - i));
+			
+			for (int j = 1; j <= i; j++){
+				System.out.print(range - (range - i)); 
+				for (int k = 1; k < j; k+=j) {
+					System.out.print(range - (range - i));
+				}
+			}
+			System.out.println();
+		}
 	}
-
+	public static void printSpaces(int numOfS) {
+		for (int i = 1; i <= numOfS; i++) {
+			System.out.printf("%-1s", " ");	
+		}
+	}
 }
