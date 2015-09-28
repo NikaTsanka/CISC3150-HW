207061bd17aefadc92bf33be3b5f5a1579776b3d
NikaTsanka <nikatsanka@outlook.com>
2015-09-28 16:34:33 -0400

    Active Commit

--- JAVA - HW4/src/MyString.java
+++ JAVA - HW4/src/MyString.java
@@ -1,9 +1,104 @@
+import java.util.Arrays;
 
 public class MyString {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	//Character array to store the passed characters.
+	private char[] chars;
 
+	public MyString(char cPass) {
+		this.chars = new char[1];
+		
+		this.chars[0] = cPass;
 	}
+	/**
+	 * Constructor to store the passed characters in
+	 * private character array.
+	 * @param charPass
+	 */
+	public MyString(char[] charsPass) {
+		this.chars = new char[charsPass.length];
+
+		for (int i = 0; i < charsPass.length; i++) {
+			this.chars[i] = charsPass[i];
+		}
+	}
+
+	public MyString(int i) {
+
+
+		char intToChar =  (char) ('0' + i);
+
+		this.chars = new char[1];
+
+		this.chars[0] = intToChar;
+	}
+
+	//works
+	public char charAt(int index){
+		return this.chars[index];
+	}
+
+	//works
+	public int length() {
+		return this.chars.length;
+	}
+
+	public MyString substring(int begin, int end) {
+		MyString subString = new MyString(
+				Arrays.copyOfRange(this.chars, begin, end));
+
+		return subString;
+	}
+
+	//works
+	public String toString() {
+		String printStr = new String(chars);
+
+		return printStr;
+	}
+
+	//not working.
+	//	public static MyString valueOf(int i) {
+	//		
+	//		MyString val = new MyString(i);
+	//		
+	//		return val;
+	//	}
+
+	//works
+//	public boolean equals(MyString s) {
+//		if (this.chars.length == s.length() &&
+//			this.chars[0] == s.charAt(0)) {
+//			return true;
+//		}
+//		return false;
+//	}
+	
+	public boolean equals(MyString s) {
+		
+		int flag = 0;
+		
+		if (this.chars.length == s.length()) {
+			for (int i = 0; i < s.length(); i++) {
+				if (this.charAt(i) == s.charAt(i)){
+					flag++;
+				}
+			}
+			if (flag == s.length()) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+		return false;
+	}
+	
+
+	//
+	public MyString getMyString() {
+		return new MyString(chars);
+	}
+
+
 
 }
--- JAVA - HW4/src/MyStringDriver.java
+++ JAVA - HW4/src/MyStringDriver.java
@@ -2,7 +2,48 @@
 public class MyStringDriver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		char[] array = { 'a', 'r', 't', 'i', 's', 't' };
+		
+		char[] array2 = { 'a', 'r', 't', 'i', 's', 't', 's' };
+		
+		MyString test = new MyString(array);
+		
+		//toString works
+		System.out.println(test);
+		
+		//charAt works
+		System.out.println(test.charAt(2));
+		
+		//length works
+		System.out.println(test.length());
+		
+		//substring works.
+		System.out.println(test.substring(1, 4));
+		
+		//getMyString
+		System.out.println(test.getMyString());
+		
+		//valueOf
+		//System.out.println(MyString.valueOf(7));
+		
+		//equals
+//		test = null;
+//		test = new MyString('s', '5');
+		
+		MyString testTo = new MyString(array2);
+		
+		if (test.equals(testTo)) {
+			System.out.println("Yes: " + test.toString() + 
+							   " equals " + testTo.toString());
+		} else {
+			System.out.println("No: " + test.toString() + 
+							   " doesn't equal " +
+							   testTo.toString());
+		}
+		
+		
+		
 
 	}
 
