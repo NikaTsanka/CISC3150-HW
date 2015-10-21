f3aeae8edc990a711a612bfeb659913f090e5010
NikaTsanka <nikatsanka@outlook.com>
2015-10-20 21:07:20 -0400

    Save

--- JAVA - HW6/src/Q1.java
+++ JAVA - HW6/src/Q1.java
@@ -1,9 +1,25 @@
-
+/**
+ * 
+ * @author Nika
+ *
+ */
 public class Q1 {
+	
+	private static int numOfArgs = 3;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		try {
+			if (args.length == numOfArgs) {
+				
+			} else {
+				throw new NotEnoughNumbersException();
+			}
+		} catch () {
+			
+		}
+		
 	}
 
 }
