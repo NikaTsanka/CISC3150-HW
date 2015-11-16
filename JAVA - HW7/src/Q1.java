e45192b64bae62bbd7e426ee815cf1f640311e19
NikaTsanka <nikatsanka@outlook.com>
2015-11-15 19:32:54 -0500

    Working Copy. Print Func doesn't work.

--- JAVA - HW7/src/Q1.java
+++ JAVA - HW7/src/Q1.java
@@ -3,52 +3,62 @@ import java.util.Scanner;
 import static java.lang.System.out;
 
 public class Q1 {
-	
+
 	private String startingPoint = " ";
-	
+
 	private File fileObj;
-	
+
 	private static Scanner myScanner = new Scanner(System.in);;
 
 	public Q1() {
-		
+
 	}
-	
+
 	public void setFile() {
-		
+
 		//First set the path
-		
-		setStartingPoint();
-		
-		fileObj = new File(startingPoint + ":\\");
-		
+
+		//setStartingPoint();
+
+		//fileObj = new File(startingPoint + ":\\");
+
+		fileObj = new File("D:\\Torrent Downloads\\Others");
+
 		out.println(fileObj.getAbsolutePath());
-		
-		
-		
-		
-	}
-	
-	public void seekAndPrint(File filePath) {
-		String[] pathList = filePath.list();
+
+		/*File[] pathList = fileObj.listFiles();
+
 		for (int i = 0; i < pathList.length; i++) {
-			if (pathList[i].d) {
-				
-			} else {
+			out.println(pathList[i].getAbsolutePath());
+		}*/
+		seekAndPrint(fileObj);
+
+	}
+
+	public void seekAndPrint(File filePath) {
+		File[] pathList = filePath.listFiles();
+
+		for (int i = 0; i < pathList.length; i++) {
+			//File tmpFileObj = new File(filePath.getPath() + pathList[i]);
+
+			if (pathList[i].isDirectory() && !(pathList[i].isHidden())) {
+				out.print("---" + pathList[i].getName());
+				out.println();
+				seekAndPrint(pathList[i]);
 
 			}
 		}
 	}
-	
+
 	//Method to set startingPoint
 	public void setStartingPoint() {
 		out.print("Which Drive: ");
-		
+
 		startingPoint = myScanner.next();
 	}
 
 	public boolean checkPath(File filePath) {
-		
+
 		//First check if it exists 
 		if (filePath.exists()) {
 			//Then check if its a file
@@ -69,11 +79,11 @@ public class Q1 {
 			return false;
 		}	
 	}
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Q1 testObj = new Q1();
-		
+
 		testObj.setFile();
 	}
 
