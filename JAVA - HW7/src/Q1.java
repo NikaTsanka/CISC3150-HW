f3539dff3e033fcea9a86f59d2898be5ed27ba63
NikaTsanka <nikatsanka@outlook.com>
2015-11-18 13:41:36 -0500

    Final Commit. Wednesday - 11/18/15 - 1:39PM

--- JAVA - HW7/src/Q1.java
+++ JAVA - HW7/src/Q1.java
@@ -1,90 +1,97 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
-import static java.lang.System.out;
 
 public class Q1 {
-
-	private String startingPoint = " ";
-
+	// Scanner Variable
+	private Scanner myScanner = new Scanner(System.in);
+	// Create the output file.
+	private File OutPutFile = new File("dir_tree.txt");
+	// PrintWriter object for outputing to a file.
+	private PrintWriter output;
+	// String variable for storing filepath.
+	private String startingPoint;
+	// The main file object.
 	private File fileObj;
-
-	private static Scanner myScanner = new Scanner(System.in);;
+	// Directory counter.
+	private int numOfDir;
 
 	public Q1() {
-
-	}
-
-	public void setFile() {
-
-		//First set the path
-
-		//setStartingPoint();
-
-		//fileObj = new File(startingPoint + ":\\");
-
-		fileObj = new File("D:\\Torrent Downloads\\Others");
-
-		out.println(fileObj.getAbsolutePath());
-
-		/*File[] pathList = fileObj.listFiles();
-
-		for (int i = 0; i < pathList.length; i++) {
-			out.println(pathList[i].getAbsolutePath());
-		}*/
-		seekAndPrint(fileObj);
-
-	}
-
-	public void seekAndPrint(File filePath) {
-		File[] pathList = filePath.listFiles();
-
-		for (int i = 0; i < pathList.length; i++) {
-			//File tmpFileObj = new File(filePath.getPath() + pathList[i]);
-
-			if (pathList[i].isDirectory() && !(pathList[i].isHidden())) {
-				out.print("---" + pathList[i].getName());
-				out.println();
-				seekAndPrint(pathList[i]);
-
-			}
+		try {
+			// Create PrintWriter object to write to the file.
+			output = new PrintWriter(OutPutFile);
+			// Call function to set the root directory.
+			setRoot();
+			// Call function to search and print.
+			seekAndprint(fileObj, 0);
+			// Output on the console.
+			System.out.println("Total number of directories: " + numOfDir);
+		} catch (IOException e) {
+			System.out.println("Yep something definitely went wrong");
+			e.printStackTrace();
+		} finally {
+			output.close();
 		}
 	}
 
-	//Method to set startingPoint
-	public void setStartingPoint() {
-		out.print("Which Drive: ");
-
+	private void setRoot() throws FileNotFoundException {
+		// First set the root directory path.
+		System.out.print("Which Drive: ");
 		startingPoint = myScanner.next();
+
+		// Create the File object
+		fileObj = new File(startingPoint + ":\\");
+
+		// Check if the drive is valid.
+		if (!checkPath(fileObj)) {
+			throw new FileNotFoundException();
+		}
+		// Inform the user which file system will be printed.
+		System.out.println(fileObj.getAbsolutePath() +
+				" file system will be printed in " + 
+				OutPutFile.getName());
 	}
 
-	public boolean checkPath(File filePath) {
+	private void seekAndprint(File fileObj, int numOfTabs) {
+		// Create a local array of files in the current directory
+		File[] pathList = fileObj.listFiles();
+		// Go through the array of files.
+		for (int i = 0; i < pathList.length; i++) {
+			// Check if it's a directory and it's not hidden.
+			if (pathList[i].isDirectory() && !(pathList[i].isHidden())) {
+				// Print tabs.
+				for (int j = 0; j < numOfTabs; j++) {
+					output.print("\t");
+				}
+				// Print the result to file.
+				output.print("[ " + pathList[i].getName() + " ] \n");
+				// Increase total count of directories. 
+				numOfDir++;
+				// Call the function and increase number of tabs.
+				seekAndprint(pathList[i], numOfTabs + 1);
+			}// End of if()
+		}// End of for()
+	}// End of seekAndprint()
 
+	private boolean checkPath(File filePath) {
 		//First check if it exists 
 		if (filePath.exists()) {
-			//Then check if its a file
-			if (filePath.isFile()) {
-				//Then check if its a directory
-				if (filePath.isDirectory()) {
-					return true;
-				} else {
-					out.println(filePath + ": is not a directory");
-					return false;
-				}
+			//Then check if its a directory
+			if (filePath.isDirectory()) {
+				return true;
 			} else {
-				out.println(filePath + ": is not a file");
+				System.out.println(filePath + " is not a directory");
 				return false;
-			}
+			}// End of nested if()
 		} else {
-			out.println(filePath + ": does not exist");
+			System.out.println(filePath + " does not exist");
 			return false;
 		}	
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Q1 testObj = new Q1();
-
-		testObj.setFile();
+		new Q1();
 	}
-
 }
