2584f584e599dd49c90a5d583472d430f2a1ba05
NikaTsanka <nikatsanka@outlook.com>
2015-11-20 15:13:42 -0500

    Small Changes.

--- JAVA - HW8/src/chat/ClientA.java
+++ JAVA - HW8/src/chat/ClientA.java
@@ -1,9 +1,25 @@
 package chat;
 
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.util.*;
+
 public class ClientA {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Set up pipes
+		try {
+			PipedOutputStream localPOS = new PipedOutputStream();
+			
+			PipedInputStream localPIS = new PipedInputStream(localPOS);
+		} catch (IOException e) {
+			// TODO: handle exception
+		}
+		
+		
+		
+		
 
 	}
 
